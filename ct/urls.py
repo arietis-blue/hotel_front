bc9def9f0035ba2ca7998ee4ead4167066d828da
arietis-blue <105206751+arietis-blue@users.noreply.github.com>
2023-08-18 03:12:03 +0900

    Delete ct directory

--- ct/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/create_image/', views.Start.as_view(), name='creative_text'),
-    path('api/check_state/', views.Poll.as_view(), name='check_state'),
-]
